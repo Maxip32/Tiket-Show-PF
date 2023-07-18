985491fff704338d59a326ebe023377edf50a95a
danielfabiani.dev <danielfabiani.dev@gmail.com>
2023-07-18 13:32:22 -0300

    realizando la NavBar 70%

--- Ticket-Show-Front/src/App.jsx
+++ Ticket-Show-Front/src/App.jsx
@@ -1,8 +1,8 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
-import Home from "./Views/Home/Home";
 import Detail from "./Views/Detail/Detail";
+import Home from "./views/Home/Home";
 
 function App() {
   return (
