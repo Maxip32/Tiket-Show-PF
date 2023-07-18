092e63e1e07fc04413609f6bb68d21ddf1d2b17c
KGabriel Perez <121875673+KennyGi0053@users.noreply.github.com>
2023-07-18 13:30:48 -0300

    Renderizado de SearhBar en Home

---
+++ Ticket-Show-Front/src/components/SearchBar/SearchBar.jsx
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+//import { useDispatch } from "react-redux";
+// importar la conexion en la action de redux cuando este lista
+
+
+const SearchBar = () => {
+   // const dispatch = useDispatch()
+    const [name, setName] = useState('')
+
+    const handleChange = (event) => {
+
+        event.preventDefault()
+        setName(event.target.value)
+    }
+
+    const handleButton = (event) => {
+        event.preventDefault()
+        if (name.trim() === "") {
+            return
+        }
+ //       dispatch(searchbyname(name))
+    }
+
+
+
+    return (
+        <div className="w-full max-w-xs m-auto">
+            <input className="border-2 border-solid border-gray-500 rounded-lg" type='text' placeholder="Search" onChange={(event) => handleChange(event)} />
+            <button className="border-2 border-solid border-gray-500 rounded-lg" type="submit" onClick={(event) => handleButton(event)} >Search</button>
+        </div>
+    )
+}
+
+export default SearchBar
