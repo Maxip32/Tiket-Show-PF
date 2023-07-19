bc154aa2449cb00b372bfad2ad72dd1157255f73
KGabriel Perez <121875673+KennyGi0053@users.noreply.github.com>
2023-07-19 17:37:00 -0300

    redux listo solo de agregar futuras actions y reducers

--- Ticket-Show-Front/src/main.jsx
+++ Ticket-Show-Front/src/main.jsx
@@ -3,20 +3,20 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import { BrowserRouter } from 'react-router-dom'
-//import { Provider } from 'react-redux'
-//import store from './redux/store'
+import { Provider } from 'react-redux'
+import store from './redux/store'
 
-// axios.defaults.baseURL = "http://localhost:3001/";
+ //axios.defaults.baseURL = "http://localhost:3001/";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   
 
- // <Provider store = {store}>
+  <Provider store = {store}>
   <React.StrictMode>
     <BrowserRouter>
       <App />
     </BrowserRouter>
   </React.StrictMode>
-//  </Provider>,
+  </Provider>,
   
 )
