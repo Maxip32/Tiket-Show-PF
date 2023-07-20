79ee5e51f73aa1a53cf69cb2dfb7bf739d682e58
KGabriel Perez <121875673+KennyGi0053@users.noreply.github.com>
2023-07-20 16:27:07 -0300

    ordenamienotos

--- Ticket-Show-Front/src/views/Home/Home.jsx
+++ Ticket-Show-Front/src/views/Home/Home.jsx
@@ -1,89 +1,97 @@
-import { useEffect, useState } from "react"
-import CardsContainer from "../../components/CardContainer/CardsContainer"
-import Hero from "../../components/Hero/Hero"
-import SearchBar from "../../components/SearchBar/SearchBar"
-import { filterByGenres, getGenres } from "../../redux/actions"
-import { useDispatch, useSelector } from 'react-redux'
-
+import { useEffect, useState } from "react";
+import CardsContainer from "../../components/CardContainer/CardsContainer";
+import Hero from "../../components/Hero/Hero";
+import SearchBar from "../../components/SearchBar/SearchBar";
+import { filterByGenres, getGenres } from "../../redux/actions";
+import { useDispatch, useSelector } from "react-redux";
 
 const Home = () => {
+  const dispatch = useDispatch();
 
-
-   const dispatch = useDispatch()
-  
-  const genres = useSelector(state => state.genres)
-  const [order, setOrder] = useState(true)
+  const genres = useSelector((state) => state.genres);
+  const [order, setOrder] = useState(true);
   //const ciudades = useSelector(state => state.ciudades)
-  
-   useEffect(() => {
-     dispatch(getGenres())
-   }, [dispatch])
 
+  useEffect(() => {
+    dispatch(getGenres());
+  }, [dispatch]);
 
   // useEffect(() => {
   // dispatch(getCiudades())
   //}, [dispatch])
 
-
-   const handleFilterGenres = (event) => {
-      dispatch(filterByGenres(event.target.value))
-    }
-  
+  const handleFilterGenres = (event) => {
+    dispatch(filterByGenres(event.target.value));
+  };
 
   //const handleFiltroCiudades = (event) => {
   //   dispatch(filtroDeCiudadesEnActions(event.target.value))
   // }
 
-
   const [date, setDate] = useState({
-    dates:'',
-  })
+    dates: "",
+  });
   const handleInputChange = (event) => {
     const { value } = event.target;
-    setDate ({
-      
+    setDate({
       dates: value,
     });
-  }
+  };
 
   const handleOrderDate = (event) => {
-    dispatch(orderByDate(event.target.value))
-    order ? setOrder(false) : setOrder(true)
-  }
+    dispatch(orderByDate(event.target.value));
+    order ? setOrder(false) : setOrder(true);
+  };
 
   return (
     <div>
       <Hero />
 
-       <SearchBar/>
+      <SearchBar />
 
-      <select className="border-2 border-solid border-gray-500 rounded-lg "  onChange={event => handleFilterGenres(event)}  defaultValue='default' >
-        <option value='default' disabled > Género musical </option>
-        {
-                genres?.map(gen => (
-                    <option value={gen.name} key={gen.id}>{gen.name}</option>
-                    
-                    ))
-                }
+      <select
+        className="border-2 border-solid border-gray-500 rounded-lg "
+        onChange={(event) => handleFilterGenres(event)}
+        defaultValue="default"
+      >
+        <option value="default" disabled>
+          {" "}
+          Género musical{" "}
+        </option>
+        {genres?.map((gen) => (
+          <option value={gen.name} key={gen.id}>
+            {gen.name}
+          </option>
+        ))}
       </select>
-    <select className="border-2 border-solid border-gray-500 rounded-lg " /* onChange={event => handleFiltroCiudades(event)} */ defaultValue='default'>
-        <option value='default' disabled > Ciudades </option>
-     </select>
-       
-       <select onChange={event => handleOrderDate(event)} defaultValue='default'>
-                <option value = 'default' disabled>Orden de Eventos</option>
-                <option value= 'desc'>Eventos más recientes</option>
-                <option value='asc'>Eventos más lejanos</option>
-       </select>
-      <input type='date' value={date.dates} name='Fecha' onChange={event => handleInputChange(event)} /> 
+      <select
+        className="border-2 border-solid border-gray-500 rounded-lg "
+        /* onChange={event => handleFiltroCiudades(event)} */ defaultValue="default"
+      >
+        <option value="default" disabled>
+          {" "}
+          Ciudades{" "}
+        </option>
+      </select>
+
+      <select
+        onChange={(event) => handleOrderDate(event)}
+        defaultValue="default"
+      >
+        <option value="default" disabled>
+          Orden de Eventos
+        </option>
+        <option value="desc">Eventos más recientes</option>
+        <option value="asc">Eventos más lejanos</option>
+      </select>
+      <input
+        type="date"
+        value={date.dates}
+        name="Fecha"
+        onChange={(event) => handleInputChange(event)}
+      />
       <CardsContainer />
-
-    
-
-
-      </div>
-  )
-
-}
-export default Home
-
+    </div>
+  );
+};
+export default Home;
