e658dc9aa42c02d4611286d9e852eec0dd1bf01d
ElGatox <sixtoledo1@gmail.com>
2023-07-20 12:07:02 -0300

    cambios en estilos

--- Ticket-Show-Front/src/components/Card/Card.jsx
+++ Ticket-Show-Front/src/components/Card/Card.jsx
@@ -3,28 +3,22 @@ import { Link } from "react-router-dom";
 
 const Card = (props) => {
   return (
-    <div className={style.allContainer}>
+    <div className=" max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
       <Link to={`detail/${props.id}`} className={style.link}>
-        <div className={style.card}>
-          <div className={style.imageContainer}>
-            <img
-              className={style.image}
-              src={props.image}
-              alt="imgagen no encontrada"
-            />
-          </div>
-          <div className={style.healtScoreContainer}>
-            <h3 className={style.healtScore}>
-              {" "}
-             Fecha: {props.date}
-            </h3>
-          </div>
-          <div className={style.infoContainer}>
-            <h3 className={style.name}>{props.name}</h3>
-            <h3 className={style.name}>{props.location}</h3>
-
-            
-          </div>
+        <div className="">
+          <img
+            className="w-60 h-40 object-contain"
+            src={props.image}
+            alt="imagen no encontrada"
+          />
+        </div>
+        <div className={style.healtScoreContainer}>
+        <h3 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white">{props.name}</h3>
+          <h3 className={style.healtScore}>Fecha: {props.date}</h3>
+        </div>
+        <div className={style.infoContainer}>
+         
+          <h3 className={style.name}>{props.location}</h3>
         </div>
       </Link>
     </div>
--- Ticket-Show-Front/src/views/Home/Home.jsx
+++ Ticket-Show-Front/src/views/Home/Home.jsx
@@ -1,17 +1,15 @@
 //import { useEffect } from "react"
 
-import CardsContainer from "../../components/CardContainer/CardsContainer"
-import Hero from "../../components/Hero/Hero"
-import SearchBar from "../../components/SearchBar/SearchBar"
+import CardsContainer from "../../components/CardContainer/CardsContainer";
+import Hero from "../../components/Hero/Hero";
+import SearchBar from "../../components/SearchBar/SearchBar";
 //import { useDispatch, useSelector } from 'react-redux'
 
-
 const Home = () => {
-
   //const dispatch = useDispatch()
   //const generos = useSelector(state => state.generos)
   //const ciudades = useSelector(state => state.ciudades)
-  
+
   // useEffect(() => {
   //   dispatch(getGeneros())
   // }, [dispatch])
@@ -23,32 +21,39 @@ const Home = () => {
   // const handleFiltroGeneros = (event) => {
   //   dispatch(filtroDeCiudadesEnActions(event.target.value))
   // }
-  
+
   //const handleFiltroCiudades = (event) => {
   //   dispatch(filtroDeCiudadesEnActions(event.target.value))
   // }
-  
+
   return (
     <div>
-        
-
       <Hero />
-       <SearchBar/>
-
-      <select className="border-2 border-solid border-gray-500 rounded-lg " /* onChange={event => handleFiltroGeneros(event)} */ defaultValue='default' >
-        <option value='default' disabled > Género musical </option>
+      <SearchBar />
+        <div className="flex  justify-center space-x-9 "> 
+        <label for="underline_select" class="sr-only">Underline select</label>
+      <select
+        className="block py-2.5 px-0 w-44 text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer "
+        /* onChange={event => handleFiltroGeneros(event)} */ defaultValue="default"
+      >
+        <option value="default" disabled>
+          {" "}
+          Género musical{" "}
+        </option>
       </select>
-    <select className="border-2 border-solid border-gray-500 rounded-lg " /* onChange={event => handleFiltroCiudades(event)} */ defaultValue='default'>
-        <option value='default' disabled > Ciudades </option>
-
-     </select>
-      <CardsContainer />
-
-    </select>
-
-
+      <select
+        className="block py-2.5 px-0 w-44 text-sm text-gray-500 bg-transparent border-0 border-b-2 border-gray-200 appearance-none dark:text-gray-400 dark:border-gray-700 focus:outline-none focus:ring-0 focus:border-gray-200 peer "
+        /* onChange={event => handleFiltroCiudades(event)} */ defaultValue="default"
+      >
+        <option value="default" disabled>
+          {" "}
+          Ciudades{" "}
+        </option>
+      </select>
       </div>
-  )
-}
+      <CardsContainer />
+    </div>
+  );
+};
 
-export default Home
+export default Home;
