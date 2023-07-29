f2412b5b3d9228c6134871d0bc02aa6fa5380f18
rodrigo Gonzalez Oliver <rodrigo.gonzalez.oliver97@gmail.com>
2023-07-29 18:44:50 -0300

    createEvent a testear

--- Tiket-Show-Back/src/routes/eventRouter.js
+++ Tiket-Show-Back/src/routes/eventRouter.js
@@ -6,6 +6,7 @@ const {
     getEvent,
     getEvents,
     getEventByName,
+    createEvent,
 } = require('../controllers/eventControllers/getControllers')
 
 
@@ -15,5 +16,7 @@ router.get('/getEvents', getEvents);
 router.get('/getEvent/:id', getEvent);
 
 router.get('/getEvent/name/:name', getEventByName);
+
+router.post('/createEvent', createEvent);
 
 module.exports = router;
