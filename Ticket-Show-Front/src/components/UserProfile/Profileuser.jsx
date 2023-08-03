8a019ed75362e9cd790017498df9ce815d9a5afb
ElGatox <sixtoledo1@gmail.com>
2023-08-02 23:22:19 -0300

    cambios en estilos

--- Ticket-Show-Front/src/components/UserProfile/Profileuser.jsx
+++ Ticket-Show-Front/src/components/UserProfile/Profileuser.jsx
@@ -120,6 +120,21 @@ export default function UserProfile() {
     }
   };
 
+  function upperLetter(inputString) {
+    if (!inputString) {
+      return "";
+    }
+
+    const wordsArray = inputString.split(" ");
+    const formattedArray = wordsArray.map((word) => {
+      const firstLetter = word.charAt(0).toUpperCase();
+      const restOfWord = word.slice(1);
+      return firstLetter + restOfWord;
+    });
+
+    return formattedArray.join(" ");
+  }
+
   const handleChangeName = () => {
     setError(null);
     if (newDisplayName.trim() === "") {
@@ -150,198 +165,111 @@ export default function UserProfile() {
 
   return (
     <>
-      <section>
-      <div class="flex flex-row">
-  <div className="border-2 w-20 h-10  basis-1/4">01</div>
-  <div className=" border-2 w-20 h-10  basis-2/4">{user.displayName}</div>
-  <div className="border-2 w-20 h-10  basis-1/4">03</div>
-    
-</div>
-<div class="flex flex-row">
-  <div className="border-2 w-20 h-10  basis-1/4">01</div>
-  <div className=" border-2 w-20 h-10  basis-2/4">{user.displayName}</div>
-  <div className="border-2 w-20 h-10  basis-1/4">03</div>
-    
-</div>
+      <section className="max-w-4xl mx-auto mt-10 bg-white shadow-2xl">
+        <div className="flex flex-col md:flex-row">
+          <div className="md:flex-shrink-0 flex justify-center items-center h-60 md:w-1/6">
+            <button
+              onClick={() =>
+                document.querySelector('input[type="file"]').click()
+              }
+              className="absolute left-30 top-40 uppercase bg-white text-primaryColor font-bold hover:shadow-md shadow text-lg rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
+            >
+              <BsPlusSquareFill />
+            </button>
+            <input
+              type="file"
+              onChange={handleImageChange}
+              className="hidden"
+            />
+            <img
+              id="imagenPrevia"
+              src={
+                preview ||
+                user.photoURL ||
+                "https://res.cloudinary.com/dhickjcbz/image/upload/v1690770100/user_r20d1h.png"
+              }
+              className="shadow-xl w-40 ml-20 rounded-full align-middle border-none"
+              alt=""
+            />
+          </div>
+          <div className="md:ml-20 h-50 py-16 md:w-1/8">
+            <h1 className="text-3xl font-bold  text-primaryColor ">
+              {upperLetter(user.displayName)}
+            </h1>
+            <h1 className="text-md"> {user.email}</h1>
+            <h1>
+              <button onClick={handleSaveChanges} className="">
+                Guardar Cambios
+              </button>
+              {error && (
+                <p className="text-red-600 text-base">Error: {error}</p>
+              )}
+            </h1>
+          </div>
+          {usersFinder?.role === "artista" ? (
+            <div className=" w-20 h-10  basis-1/4">
+              <p className="text-xs font-bold flex text-primaryColor justify-center items-center">
+                Nombre Artístico :
+              </p>
+              <p className="flex text-LightGrayText justify-center items-center">
+                {upperLetter(datosartist?.nameBand)}
+              </p>
+            </div>
+          ) : null}
+          {usersFinder?.role === "artista" ? (
+            <div className="w-20 h-10  basis-1/4 ">
+              <p className="text-xs font-bold flex text-primaryColor justify-center items-center ">
+                Año de Creacion:
+              </p>
+              <p className="flex  justify-center items-center ">
+                {datosartist?.yearCreation}
+              </p>
+            </div>
+          ) : null}
+        </div>
+        <div className="flex flex-col  ml-10 md:flex-row">
+          <div className="md:h-20 md:w-1/4">
+            <p className="flex justify-start font-bold text-xs">
+              Cambiar Nombre:
+            </p>
+            {user?.displayName ? (
+              <>
+                <input
+                  type="text"
+                  className="ml-2 w-2/8 text-xs bg-LightText rounded-xl text-black"
+                  value={newDisplayName}
+                  onChange={(e) => setNewDisplayName(e.target.value)}
+                />
+                <button
+                  onClick={handleChangeName}
+                  className=" flex flex-col  text-xs"
+                >
+                  Guardar
+                </button>
+              </>
+            ) : (
+              <p>Cargando...</p>
+            )}
+            {error && <p className="text-red-600 text-base">Error: {error}</p>}
+          </div>
+          <div className="md:w-1/4"></div>
+          <div className="md:w-1/4"></div>
+        </div>
         <div>
           <div>
-            <div className="">
-              <div className="">
-                <div className="">
-                  <div className="relative">
-                    <img
-                      id="imagenPrevia" // O "vistaPreviaImagen" u otro nombre
-                      src={
-                        preview ||
-                        user.photoURL ||
-                        "https://res.cloudinary.com/dhickjcbz/image/upload/v1690770100/user_r20d1h.png"
-                      }
-                      className="shadow-xl w-40 rounded-full align-middle border-none"
-                      alt=""
-                    />
-                    <div className="flex  absolute top-1">
-                      <button
-                        onClick={() =>
-                          document.querySelector('input[type="file"]').click()
-                        }
-                        className="bg-pink-500 active:bg-pink-600 uppercase text-white font-bold hover:shadow-md shadow text-xs px-4 py-2 rounded outline-none focus:outline-none sm:mr-2 mb-1 ease-linear transition-all duration-150"
-                      >
-                        <BsPlusSquareFill />
-                      </button>
-                      <input
-                        type="file"
-                        onChange={handleImageChange}
-                        className="hidden"
-                      />
-                    </div>
-                    <button onClick={handleSaveChanges} className="">
-                      Guardar Cambios
-                    </button>
-                    {error && (
-                      <p className="text-red-600 text-base">Error: {error}</p>
-                    )}
-                    <span></span>
-                  </div>
-                </div>
-                <div className="">
-                
-                </div>
-                <div className="">
-                  <div className="">
-                    <div className="">
-                      {/* <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">22</span><span className="text-sm text-blueGray-400">Friends</span> 
-                    </div>
-                    <div className="mr-4 p-3 text-center">
-                      {/* <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">10</span><span className="text-sm text-blueGray-400">Photos</span>*/}
-                    </div>
-                    <div className="">
-                      {/*  <span className="text-xl font-bold block uppercase tracking-wide text-blueGray-600">89</span><span className="text-sm text-blueGray-400">Comments</span>*/}
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="">
-                <divc>
-                  <h3 className="">
-                    {user.displayName}
-                  </h3>
-                </divc>
-                <div className=" ">
-                  <i className=""></i>
-                  {user.email}
-                </div>
-                {/* {* si es artista*} */}
-                {/* {usersFinder?.role === "artista" ? (
-                  <div className="flex-col">
-                    <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold ">
-                      <p className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold ">
-                        Nombre de Artista :
-                      </p>
-                      <p className="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400">
-                        {datosartist?.nameArtist}
-                      </p>
-                    </div>
-                  </div>
-                ) : null} */}
-                {usersFinder?.role === "artista" ? (
-                  <div className="flex-col">
-                    <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold ">
-                      <p className="text-sm leading-normal mt-0 mb-2 text-primaryColor text-blueGray-400 font-bold ">
-                        Nombre Artístico :
-                      </p>
-                      <p className="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400">
-                        {datosartist?.nameBand}
-                      </p>
-                    </div>
-                  </div>
-                ) : null}
-                {usersFinder?.role === "artista" ? (
-                  <div className="flex-col">
-                    <div className="text-sm leading-normal mt-0 mb-2 text-blueGray-400 font-bold ">
-                      <p className="text-sm leading-normal mt-0 mb-2 text-primaryColor text-blueGray-400 font-bold ">
-                        Año de creacion:
-                      </p>
-                      <p className="fas fa-map-marker-alt mr-2 text-lg text-blueGray-400">
-                        {datosartist?.yearCreation}
-                      </p>
-                    </div>
-                  </div>
-                ) : null}
-                <div>
-                  <div className="">
-                    <div className="">
-                      <p className="">
-                        Cambiar Nombre:
-                      </p>
-                      {user?.displayName ? (
-                        <>
-                          <input
-                            type="text"
-                            className="text-xl bg-LightText rounded-3xl text-black px-6"
-                            value={newDisplayName}
-                            onChange={(e) => setNewDisplayName(e.target.value)}
-                          />
-                          <button
-                            onClick={handleChangeName}
-                            className=""
-                          >
-                            Guardar
-                          </button>
-                        </>
-                      ) : (
-                        <p>Cargando...</p>
-                      )}
-                      {error && (
-                        <p className="text-red-600 text-base">Error: {error}</p>
-                      )}
-                    </div>
-                  </div>
-                </div>
-              </div>
-              <div className="mt-10 py-10 border-t border-blueGray-200 text-center">
-                <div className="flex flex-wrap justify-center">
-                  <div className="flex flex-row w-full">
-                    <div className="flex-1 px-4">
-                      <p className="mb-4 text-lg leading-relaxed text-blueGray-700 flex">
-                        {<MyShopping user_id={user} />}
-                      </p>
-                      <a
-                        href="#pablo"
-                        className="font-normal text-pink-500"
-                      ></a>
-                    </div>
+            <div className="mt-10 py-10 border-t border-blueGray-200 text-center">
+              <div className="flex flex-wrap justify-center">
+                <div className="flex flex-row w-full">
+                  <div className="flex-1 px-4">
+                    <p className="mb-4 text-lg leading-relaxed text-blueGray-700 flex">
+                      {<MyShopping user_id={user} />}
+                    </p>
+                    <a href="#pablo" className="font-normal text-pink-500"></a>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-
-          <footer className="relative bg-blueGray-200 pt-8 pb-6 mt-8">
-            <div className="container mx-auto px-4">
-              <div className="flex flex-wrap items-center md:justify-between justify-center">
-                <div className="w-full md:w-6/12 px-4 mx-auto text-center">
-                  <div className="text-sm text-blueGray-500 font-semibold py-1">
-                    {" "}
-                    <a
-                      href="https://www.creative-tim.com/product/notus-js"
-                      className="text-blueGray-500 hover:text-gray-800"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    ></a>{" "}
-                    <a
-                      href="https://www.creative-tim.com"
-                      className="text-blueGray-500 hover:text-blueGray-800"
-                      target="_blank"
-                      rel="noopener noreferrer"
-                    >
-                      {" "}
-                    </a>
-                    .
-                  </div>
-                </div>
-              </div>
-            </div>
-          </footer>
         </div>
       </section>
     </>
