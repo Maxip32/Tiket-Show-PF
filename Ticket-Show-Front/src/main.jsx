1ec5ef42ea29ea151bca6f09677dc1d2f978de80
Maxip32 <110627331+Maxip32@users.noreply.github.com>
2023-08-08 12:01:41 -0300

    Update main.jsx

--- Ticket-Show-Front/src/main.jsx
+++ Ticket-Show-Front/src/main.jsx
@@ -14,7 +14,7 @@ import "slick-carousel/slick/slick-theme.css";
 
 import axios from "axios";
    //axios.defaults.baseURL = "http://localhost:3001/";
- axios.defaults.baseURL = "https://ticketshow-n0gj.onrender.com/";
+ axios.defaults.baseURL = "https://tiket-show-pf-production.up.railway.app/";
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <Provider store={store}>
